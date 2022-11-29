9b4e38791b1b6f0eb099890e8ff2c8c3d0958d88
smerch <stas@belochka.kz>
2022-11-30 04:57:41 +0600

    npm install -D prettier prettier-plugin-tailwindcss https://tailwindcss.com/docs/guides/vite#vue https://github.com/tailwindlabs/prettier-plugin-tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app" class="flex flex-col items-center justify-center min-h-screen gap-4">
-    <HelloWorld msg="Hello from Tailwind inside VueJS"/>
+  <div
+    id="app"
+    class="flex flex-col items-center justify-center min-h-screen gap-4"
+  >
+    <HelloWorld msg="Hello from Tailwind inside VueJS" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from "./components/HelloWorld.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-  }
+  },
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <h1 class="text-5xl font-bold text-purple-500">{{msg}}</h1>
-    <button class="btn-blue">
+    <h1 class="text-5xl font-bold text-purple-500">{{ msg }}</h1>
+    <button class="btn-blue">Click me</button>
+    <button
+      class="rounded-lg bg-green-500 px-4 py-2 font-semibold text-white shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+    >
       Click me
     </button>
-    <button class="px-4 py-2 font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-      Click me
-    </button>
-    <button class="py-2 px-4 bg-[#7e7e7e] text-[#000] font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+    <button
+      class="rounded-lg bg-[#7e7e7e] py-2 px-4 font-semibold text-[#000] shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+    >
       I'm rendered with JIT syntax for colors 👋
     </button>
   </div>
@@ -15,12 +17,11 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
