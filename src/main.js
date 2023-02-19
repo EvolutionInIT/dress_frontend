cf10a763201e62d2fb5e501347c6cd8d25ba0e9e
smerch <stas@belochka.kz>
2023-02-19 18:14:12 +0600

    add i18n, vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import router from "./router/index";
 import axios from "axios";
+import i18n from "./i18n";
 import "virtual:windi.css";
 
 /*добавляет ссылку на роутер в каждый стор пинии*/
@@ -10,10 +12,7 @@ pinia.use(({ store }) => {
   store.router = markRaw(router);
 });
 
-const app = createApp(App)
-  //.use(router)
-  .use(pinia);
-//.use(i18n)
+const app = createApp(App).use(router).use(pinia).use(i18n);
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost/api";
 
