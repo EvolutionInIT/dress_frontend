f0ce0207e5d15fec2138d662e9f73ab1572d6cf5
smerch <stas@belochka.kz>
2023-02-19 17:57:32 +0600

    add axios and pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
+import axios from "axios";
 import "virtual:windi.css";
 
-createApp(App).mount("#app");
+/*добавляет ссылку на роутер в каждый стор пинии*/
+const pinia = createPinia();
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+
+const app = createApp(App)
+  //.use(router)
+  .use(pinia);
+//.use(i18n)
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost/api";
+
+app.mount("#app");
